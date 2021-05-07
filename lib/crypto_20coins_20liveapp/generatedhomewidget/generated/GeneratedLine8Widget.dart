025a9153b7058f4d623315d2136f4f1253c24af1
FlutLab.io <>
2021-05-07 23:18:26 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/generatedhomewidget/generated/GeneratedLine8Widget.dart
+++ lib/crypto_20coins_20liveapp/generatedhomewidget/generated/GeneratedLine8Widget.dart
@@ -8,7 +8,7 @@ class GeneratedLine8Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 17.0,
+      width: MediaQuery.of(context).size.width,
       height: 0.0,
       child: SvgWidget(painters: [
         SvgPathPainter.stroke(
