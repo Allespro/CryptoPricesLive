818f6ff5f3aeab4825b02ed50c308b3122685e19
FlutLab.io <>
2021-05-08 02:06:20 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/pricemachine.dart
+++ lib/crypto_20coins_20liveapp/pricemachine.dart
@@ -1,48 +1,9 @@
-import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:flutter/foundation.dart';
-
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({
-    @required this.userId,
-    @required this.id,
-    @required this.title,
-    @required this.body,
-  });
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'] as int,
-      id: json['id'] as int,
-      title: json['title'] as String,
-      body: json['body'] as String,
-    );
-  }
-}
-
-class HttpService {
-  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
-
-  Future<List<Post>> getPosts() async {
-    Response res = await get(postsURL);
-
-    if (res.statusCode == 200) {
-      List<dynamic> body = jsonDecode(res.body);
-
-      List<Post> posts = body
-        .map(
-          (dynamic item) => Post.fromJson(item),
-        )
-        .toList();
-
-      return posts;
-    } else {
-      throw "Unable to retrieve posts.";
-    }
-  }
+import 'package:http/http.dart' as http;
+var client = http.Client();
+try {
+  var uriResponse = await client.post(Uri.parse('https://example.com/whatsit/create'),
+      body: {'name': 'doodle', 'color': 'blue'});
+  print(await client.get(uriResponse.bodyFields['uri']));
+} finally {
+  client.close();
 }
