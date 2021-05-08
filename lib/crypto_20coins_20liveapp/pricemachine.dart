90c9ba7c2311a95fefbc4a83cc9e3313fd6bb547
FlutLab.io <>
2021-05-08 02:13:33 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/pricemachine.dart
+++ lib/crypto_20coins_20liveapp/pricemachine.dart
@@ -1,14 +1,41 @@
-import 'package:http/http.dart' as http;
 
-void get_price(){
-  var client = http.Client();
-try {
-  var uriResponse = await client.post(Uri.parse('https://example.com/whatsit/create'),
-      body: {'name': 'doodle', 'color': 'blue'});
-  print(await client.get(uriResponse.bodyFields['uri']));
-} finally {
-  client.close();
+class Country {
+  final String name;
+  final String capital;
+  final String region;
+
+  Country({this.name, this.capital, this.region});
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      name: json['name'],
+      capital: json['capital'],
+      region: json['region'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Country{name: $name, capital: $capital, region: $region)';
+  }
 }
 
+Future<Country> getCountryByName  (String name) async {
+  Country country;
+  try {
+    final url = 'https://restcountries.eu/rest/v2/name/$name';
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+
+      country = Country.fromJson(json.decode(response.body)[0]);
+    }
+    else
+      print("error: ${response.statusCode}");
+  }
+  catch (e) {
+    print(e);
+  }
+    print(country.toString());
+    return country;
 }
-get_price();
