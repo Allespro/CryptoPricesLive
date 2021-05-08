b8ce320952693cdf13790f49848fa2c91ee42c00
FlutLab.io <>
2021-05-08 01:13:01 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/pricemachine.dart
+++ lib/crypto_20coins_20liveapp/pricemachine.dart
@@ -1,17 +1,19 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class GetCoinPrices {
   String coins;
   String fiat;
   GetCoinPrices(this.coins, this.fiat);
-  void display() {
+  Future display() async {
     var queryParameters = {
       'ids': coins,
       'vs_currencies2': fiat,
       'include_24hr_change': 'true',
     };
     var uri = Uri.https('api.coingecko.com', '/api/v3/simple/price', queryParameters);
-    var response = http.get(uri);
+    var response = await http.get(uri);
     String json = response.body;
     print(json);
   }
