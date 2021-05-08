1aa36521cbc9e4eb56c44ca100f056e6b77ea496
FlutLab.io <>
2021-05-08 01:47:49 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/generatedhomewidget/GeneratedHomeWidget.dart
+++ lib/crypto_20coins_20liveapp/generatedhomewidget/GeneratedHomeWidget.dart
@@ -10,6 +10,26 @@ import 'package:flutterapp/crypto_20coins_20liveapp/generatedhomewidget/generate
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({@required this.userId, @required this.id, @required this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
 Future<Album> fetchAlbum() async {
   final response = await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
 
