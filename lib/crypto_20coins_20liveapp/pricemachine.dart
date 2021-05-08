9172cf029417e0811807e2942bd30ced67e17631
FlutLab.io <>
2021-05-08 01:57:16 +0000

    Autosave

--- lib/crypto_20coins_20liveapp/pricemachine.dart
+++ lib/crypto_20coins_20liveapp/pricemachine.dart
@@ -1,24 +1,34 @@
-import 'dart:async';
-import 'dart:io';
+import 'dart:convert';
+import 'package:flutterhttpgetpost/get_prod.dart';
+import 'package:http/http.dart' as http;
 
-
-/*
-class GetCoinPrices {
-  String coins;
-  String fiat;
-  GetCoinPrices(this.coins, this.fiat);
-  void display() {
-    var queryParameters = {
-      'ids': coins,
-      'vs_currencies2': fiat,
-      'include_24hr_change': 'true',
+class servicewrapper {
+  var baseurl = "http://androidtutorial.blueappsoftware.com//";
+  var apifolder = "webapi/";
+// see complete url --
+// http://androidtutorial.blueappsoftware.com/webapi/get_jsondata.php
+  Future<get_prod> getProdCall() async {
+    var url = baseurl + apifolder + "get_jsondata.php";
+    final body = {
+      'language': 'default',
+      'securecode': '123'
     };
-    var uri = Uri.https('api.coingecko.com', '/api/v3/simple/price', queryParameters);
-    var response = await http.get(uri);
-    String json = response.body;
-    print(json);
-    print('coins');
+    final bodyjson = json.encode(body);
+    // pass headers parameters if any
+    final response = await http.post(url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: bodyjson);
+    print(" url call from " + url);
+    if (response.statusCode == 200) {
+      print('url hit successful' + response.body);
+      String data = response.body;
+      print(' prod name - ' + jsonDecode(data)['Information'][0]['name']);
+      return get_prod.fromJson(json.decode(response.body));
+    } else {
+      print('failed to get data');
+      throw Exception('Failed to get data');
+    }
   }
 }
-*/
-
